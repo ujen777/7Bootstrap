73b2b3914b69832cdcd67e7b9cee7a2f6a09941d
ujen777 <46864191+ujen777@users.noreply.github.com>
2019-03-21 02:23:45 +0300

    <ok

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 $(document).ready(function() {
   $('#button').click(function() {
-    $('.empty').hide();
     var text1 = $('#one').val();
     var text2 = $('#two').val();
     if (text1 && text2) {
-    $('#list').append(
-      '<h5 class="card-title">'+
-        text1+'&nbsp'+
-        '<button class="btn btn-outline-secondary btn-sm switch">'+
-        '&#8595;'+'</button>'+'&nbsp'+
-        '<button class="btn btn-danger btn-sm clear-button">'+
-        '&#10008;'+'</button>'+
-      '</h5>'+
-      '<p class="card-text">'+
-        text2+
-      '</p>');
-    $('.switch').off();
-    }
-    else {
+      $('#empty').hide();
+      $('#list').append(
+      '<div class="card">'+
+        '<div class="card-body">'+
+          '<h5 class="card-title">'+            
+
+            '<button class="btn btn-danger btn-sm float-right clear-button">'+
+            '&#10008;'+
+            '</button>'+
+
+            '<button class="btn btn-outline-secondary btn-sm float-right switch">'+
+            '&#8595;'+
+            '</button>'+
+            text1+
+
+          '</h5>'+
+          '<p class="card-text">'+
+            text2+
+          '</p>'+
+        '</div>'+
+      '</div>');
+      $('.switch').off();
+    } else {
       alert('Заполните форму');
     }
 
     $('.clear-button').on('click', function(event) {
-			$(this).parents('.card-title').remove();
-		});
+      $(this).parent().parent().parent().remove();
+    });
 
     $('.switch').click(function() {
       var ok = $(this).parent();
